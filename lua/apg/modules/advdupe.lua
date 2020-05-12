15f98fa09aad8fbeab1d5db2a50da029966db21d
NoSharp <33969918+NoSharp@users.noreply.github.com>
2020-05-12 22:33:59 +0100

    Update advdupe.lua

--- lua/apg/modules/advdupe.lua
+++ lua/apg/modules/advdupe.lua
@@ -2,31 +2,24 @@ APG.Detours = APG.Detours or {}
 
 if AdvDupe2.InitPastingQueue then 
 	APG.Detours["AdvDupePastingQueue"] = APG.Detours["AdvDupePastingQueue"] or AdvDupe2.InitPastingQueue
-	APG.Detours["AdvDupePaste"] = APG.Detours["AdvDupePaste"] or AdvDupe2.duplicator.Paste
-	
 	function AdvDupe2.InitPastingQueue(Player, PositionOffset, AngleOffset, OrigPos, Constrs, Parenting, DisableParents, DisableProtection)
 		local maxScale = APG.cfg[ "maxScale" ].value
 		for k,v in pairs(Player.AdvDupe2.Entities) do 
 			if v.ModelScale and v.ModelScale > maxScale then
 				v.ModelScale = maxScale
 			end
-		end 
+		end
 
-		APG.Detours["AdvDupePastingQueue"](Player, PositionOffset, AngleOffset, OrigPos, Constrs, Parenting, DisableParents, DisableProtection)
-	end
-
-	function AdvDupe2.duplicator.Paste( Player, EntityList, ConstraintList, Position, AngleOffset, OrigPos, Parenting)
-		if ConstraintList && APG.cfg["ropeSpam"].value then
-			for k, Constraint in pairs( ConstraintList ) do
-				if(Constraint.type ~= "Rope") then continue end
-				Entity = CreateConstraintFromTable( Constraint, CreatedEntities, EntityList, Player )
-				if(IsValid(Entity))then
-					table.insert( CreatedConstraints, Entity )
+		if Player.AdvDupe2.Constraints then
+			//print("test")
+			for k, Constraint in pairs( Player.AdvDupe2.Constraints ) do
+				if(Constraint.Type == "Rope") then //table.remove( Player.AdvDupe2.Constraints, k) end
+					Player.AdvDupe2.Constraints[k] = nil
 				end
 			end
 		end
-
-		APG.Detours["AdvDupePaste"]( Player, EntityList, ConstraintList, Position, AngleOffset, OrigPos, Parenting)
-	end
 	
+		APG.Detours["AdvDupePastingQueue"](Player, PositionOffset, AngleOffset, OrigPos, Constrs, Parenting, DisableParents, DisableProtection)
+	end
+
 end
